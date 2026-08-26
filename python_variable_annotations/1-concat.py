74081aa3b75af291b1456e1115b5d556c5b15136
MN7 <dutymastr@outlook.com>
2026-08-26 14:13:43 +0300

    Holberton Task

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""add - sum a and b"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function to add operator ..................................."""
+    return str1 + str2
