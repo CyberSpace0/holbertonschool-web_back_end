455ba1fdd6116f93b7455426e97543458460d970
MN7 <dutymastr@outlook.com>
2026-09-08 14:50:56 +0300

    Holberton Task

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -12,18 +12,15 @@ def index_range(page, page_size) -> Tuple[int, int]:
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initializes a Server instance
-        """
+        """Initializes a Server instance"""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -31,16 +28,12 @@ class Server:
             self.__dataset = dataset[1:]
         return self.__dataset
 
-    """Returns the appropriate page of the dataset (i.e. the correct list of rows)
-                """
+    """Returns the appropriate page of the dataset (i.e. the correct list of rows)"""
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Returns the appropriate page of the dataset (i.e. the correct list of rows)
-                """
-        assert isinstance(
-            page, int) and page > 0
-        assert isinstance(
-            page_size, int) and page_size > 0
+        """Returns the appropriate page of the dataset (i.e. the correct list of rows)"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start, end = index_range(page, page_size)
         data = self.dataset()
