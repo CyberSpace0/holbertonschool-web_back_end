d2ed92d4e38da35be75e478c65b58c460ea6ccfa
MN7 <dutymastr@outlook.com>
2026-09-08 14:39:53 +0300

    Holberton Task

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
-"""Simple helper functionSimple helper functionSimple helper functionSimple helper functionSimple helper function"""
+"""Simple helper function."""
 
-def index_range(page: int, page_size: int) -> tuple:
-    """Return a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters.
+from typing import Tuple
 
-    Args:
-        page (int): The current page number (1-indexed).
-        page_size (int): The number of items per page.
 
-    Returns:
-        tuple: A tuple containing the start index and end index.
-    """
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple containing the start and end indexes."""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
+
     return (start_index, end_index)
