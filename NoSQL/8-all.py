71ce774ae7fec21f41f9e2e34329c733dd2f7350
MN7 <dutymastr@outlook.com>
2026-09-10 14:38:45 +0300

    Holberton Task

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -5,5 +5,4 @@ from pymongo import MongoClient
 
 def list_all(mongo_collection) -> None:
     """List all documents in the specified MongoDB collection."""
-    for document in mongo_collection.find():
-        print(document)
+    return mongo_collection.find()
