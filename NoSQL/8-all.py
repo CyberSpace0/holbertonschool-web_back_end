cbc3577e7945100fafa6a33fe3ca105c4b886088
MN7 <dutymastr@outlook.com>
2026-09-10 14:36:04 +0300

    Holberton Task

---
+++ NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""List all databases in MongoDB."""
+
+from pymongo import MongoClient
+
+def list_all(mongo_collection) -> None:
+    """List all databases available in the MongoDB server."""
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    databases = client.list_database_names()
+
+    for database in databases:
+        print(database)
+
+
+if __name__ == "__main__":
+    list_all()
