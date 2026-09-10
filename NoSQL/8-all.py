bd48cbb7f520fac4c605acabdd76da5f674b7608
MN7 <dutymastr@outlook.com>
2026-09-10 14:37:27 +0300

    Holberton Task

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,9 +4,6 @@
 from pymongo import MongoClient
 
 def list_all(mongo_collection) -> None:
-    """List all databases available in the MongoDB server."""
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    databases = client.list_database_names()
-
-    for database in databases:
-        print(database)
+    """List all documents in the specified MongoDB collection."""
+    for document in mongo_collection.find():
+        print(document)
